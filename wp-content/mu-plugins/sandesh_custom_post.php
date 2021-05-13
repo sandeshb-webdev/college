151b1be3d4b9767b57da6b70aaa5ede081ea6c86
Sandesh Budhahtoki <sandeshbyte@gmail.com>
2021-05-13 12:51:28 +0530

    added programs

--- wp-content/mu-plugins/sandesh_custom_post.php
+++ wp-content/mu-plugins/sandesh_custom_post.php
@@ -3,6 +3,7 @@
 
 // Add custom post type like events or etc on wordpress.
 function sandesh_own_custom_event(){
+  // EVENT POST TYPE
   register_post_type('events',array(
     'supports' => array('title', 'editor', 'excerpt'),
     'rewrite' => array('slug' => 'event'),
@@ -12,9 +13,25 @@ function sandesh_own_custom_event(){
     'labels' => array(
       'name' => 'Event',
       'add_new_item' => 'Add new Event',
-      'edit_item' => 'Edit Offer',
+      'edit_item' => 'Edit Event',
       'all_items' => 'All Events',
       'singular_name' => 'Event'
+    )
+  ));
+
+  // PROGRAM POST TYPE
+  register_post_type('program',array(
+    'supports' => array('title', 'editor'),
+    'rewrite' => array('slug' => 'programs'),
+    'has_archive' => true,
+    'public' => true,
+    'menu_icon' => 'dashicons-awards',
+    'labels' => array(
+      'name' => 'Programs',
+      'add_new_item' => 'Add new Program',
+      'edit_item' => 'Edit Program',
+      'all_items' => 'All Programs',
+      'singular_name' => 'Program'
     )
   ));
 
