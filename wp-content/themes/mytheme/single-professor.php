c3880e1ff6d74d29bd4522b1334124070bf2fb85
Sandesh Budhahtoki <sandeshbyte@gmail.com>
2021-05-14 17:50:30 +0530

    show custom professors related to the field

--- wp-content/themes/mytheme/single-professor.php
+++ wp-content/themes/mytheme/single-professor.php
@@ -32,7 +32,7 @@
             $relatedPrograms = get_field('related_programs');
               if($relatedPrograms){
               echo '<hr class = "section-break">';
-              echo '<h2 class="headline headline--medium">Related Programs</h2>';
+              echo '<h2 class="headline headline--medium">Subject(s) Taught</h2>';
               echo '<ul class="link-list min-list">';
               foreach($relatedPrograms as $program){ ?>
                 <li><a href="<?php echo get_the_permalink($program); ?>"> <?php echo get_the_title($program);?></a></li>
--- wp-content/themes/mytheme/single-program.php
+++ wp-content/themes/mytheme/single-program.php
@@ -29,7 +29,32 @@
           </div>
           <div class="generic-content">
              <p><?php echo the_content() ?></p>
-             <?php 
+
+          <?php 
+             $relatedProfessors = new WP_Query(array(
+              'posts_per_page' => -1, // this value shows alll the post, bettter than putting 30,40 like that
+              'post_type' => 'professor', // Calling events which is our custom post
+              'orderby' => 'title',  // Default is by date posted
+              'order' => 'ASC',
+              'meta_query' => array(
+                array(
+                    'key' => 'related_programs',
+                    'compare' => 'LIKE',
+                    'value' => '"'.get_the_ID().'"'
+                )
+              )
+            ));
+            if($relatedProfessors -> have_posts()){
+                echo '<hr classs = "section-break">';
+                echo '<h2 class="headline headline--medium"> ' .get_the_title().' Professors </h2>';
+                while($relatedProfessors -> have_posts() ){
+                     $relatedProfessors -> the_post();?>
+                     <li><a href="<?php the_permalink();?>"><?php the_title();?></a></li>
+              <?php }; }
+
+            wp_reset_postdata(); // Use it between two custom queries most of the times to avoid errors , i.e ID being taken by first one etc type of error
+
+          
             $today = date('Ymd'); // date format same as custom field date format
             $homepageEvents = new WP_Query(array(
               'posts_per_page' => -1, // this value shows alll the post, bettter than putting 30,40 like that
@@ -57,19 +82,19 @@
                 while($homepageEvents -> have_posts() ){
                      $homepageEvents -> the_post();?>
                     <div class="event-summary">
-                    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php
-                        $eventDate = new DateTime(get_field('event_date'));
-                        echo $eventDate->format('M')?> </span>
-                        <span class="event-summary__day"><?php
-                        echo $eventDate->format('d')?></span>
-                    </a>
-                    <div class="event-summary__content">
-                        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
-                        <p><?php if(has_excerpt()){
-                                echo get_the_excerpt();
-                        }else echo wp_trim_words(get_the_content(), 20);?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
-                    </div>
+                      <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+                          <span class="event-summary__month"><?php
+                          $eventDate = new DateTime(get_field('event_date'));
+                          echo $eventDate->format('M')?> </span>
+                          <span class="event-summary__day"><?php
+                          echo $eventDate->format('d')?></span>
+                      </a>
+                      <div class="event-summary__content">
+                          <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
+                          <p><?php if(has_excerpt()){
+                                  echo get_the_excerpt();
+                          }else echo wp_trim_words(get_the_content(), 20);?> <a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
+                      </div>
                     </div>
                 
             
