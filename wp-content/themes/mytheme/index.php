766567de0990fdc7439370b263f8294ead11c09c
Sandesh Budhahtoki <sandeshbyte@gmail.com>
2021-05-17 19:03:28 +0530

    full dynamic all page banners

--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,16 +1,11 @@
 <?php
-  get_header();?>
+  get_header();
+  pageBanner(array(
+    'title' => 'Welcome to blog',
+    'subtitle' => 'Keep up with latest'
+  ))
+  ?>
 
-  <div class="page-banner">
-  <div class="page-banner__bg-image"
-    style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg');?>);"></div>
-  <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title">Welcome to blog</h1>
-    <div class="page-banner__intro">
-      <p>Keep up with latest</p>
-    </div>
-  </div>
-</div>
 
 <div class="container container--narrow page-section"> 
   <?php 
